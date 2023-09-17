3a5244692c44dfa5666a1b1d61bc68573b65c6ba
honestyman <leel50296@gmail.com>
2023-09-17 15:52:07 +0300

    Some HTML

--- index.php
+++ index.php
@@ -5,9 +5,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>The Weather App</title>
 </head>
 
 <body>
+    <h1>Weather App</h1>
+    <form method="post">
+        <input type="text" name="city">
+        <input type="submit" name="submit" value="Check weather">
+    </form>
 </body>
 
 </html>
