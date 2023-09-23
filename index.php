5ba215553983224a38ccce4da76e8041bc84b0e4
honestyman <leel50296@gmail.com>
2023-09-23 17:17:16 +0300

    Updates

--- index.php
+++ index.php
@@ -42,8 +42,7 @@
 
     }else if(isset($_POST["submit"]) && !empty($_POST["city"])){
             $city = $_POST["city"];
-            //$API_KEY = "API_KEY";
-            $API_KEY = "19fdeaeefe22a24143070b3cf1b420dd";
+            $API_KEY = "API_KEY";
             $API = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$API_KEY";
             $API_DATA = file_get_contents($API);
             //print_r($API_DATA);
