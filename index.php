c0a682ae3f92d9effb738739d34e3c0dd457b1f9
honestyman <leel50296@gmail.com>
2023-09-30 15:01:38 +0300

    Layout

--- index.php
+++ index.php
@@ -42,8 +42,7 @@ if(isset($_POST["submit"])){
 
     }else{
         $city = $_POST["city"];
-        //$API_KEY = "API_KEY";
-        $API_KEY = "19fdeaeefe22a24143070b3cf1b420dd";
+        $API_KEY = "API_KEY";
         $API = "https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$API_KEY";
         $API_DATA = @file_get_contents($API);
         //$weather = json_decode($API_DATA, true);
