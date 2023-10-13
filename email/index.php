7f9c9240726804850af7d381f61f3c05e4bf86df
honestyman <leel50296@gmail.com>
2023-10-13 09:52:49 +0300

    CSS

--- email/index.php
+++ email/index.php
@@ -11,7 +11,7 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class="col-md-4 offset-md-4">
+            <div class="col-md-4 offset-md-4 mail-form">
                 <h2 class="text-center">Send message</h2>
                 <p class="text-center">Send e-mail to anyone from your Localhost</p>
                 <form action="#" method="POST" autocomplete="off">
@@ -22,7 +22,7 @@
                         <input type="text" class="form-control" placeholder="Subject">
                     </div>
                     <div class="form-group">
-                        <textarea name="" cols="30" rows="10" class="form-control" placeholder="Enter your message"></textarea>
+                        <textarea name="" cols="30" rows="10" class="form-control textarea" placeholder="Enter your message"></textarea>
                     </div>
                     <div class="form-group">
                         <input type="submit" class="form-control button btn-primary" value="Send">
