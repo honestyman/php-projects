cf72d206f94629a2ae94b743a600c8d7905416e6
honestyman <leel50296@gmail.com>
2023-10-13 12:16:37 +0300

    Final commit

--- email/index.php
+++ email/index.php
@@ -14,18 +14,48 @@
             <div class="col-md-4 offset-md-4 mail-form">
                 <h2 class="text-center">Send message</h2>
                 <p class="text-center">Send e-mail to anyone from your Localhost</p>
-                <form action="#" method="POST" autocomplete="off">
+                <?php
+                if(isset($_POST["send"])){
+                    $recipient = $_POST["email"];
+                    $subject = $_POST["subject"];
+                    $message = $_POST["message"];
+                    $sender = "From: me@example.com";
+
+                    if(empty($recipient || $subject || $message)){
+                        ?>
+                        <div class="alert alert-danger text-center">
+                            <?php echo "All input fields are required!"; ?>
+                        </div>
+                        <?php
+                    }else{
+                        if(mail($recipient, $subject, $message, $sender)){
+                            ?>
+                            <div class="alert alert-success text-center">
+                                <?php echo "Your e-mail to " . $recipient . " has been sent successfully!"; ?>
+                            </div>
+                            <?php
+                        }else{
+                            ?>
+                            <div class="alert alert-danger text-center">
+                                <?php echo "Something went wrong! Failed to send your e-mail!"; ?>
+                            </div>
+                            <?php
+                        }
+                    }
+                }
+                ?>
+                <form action="index.php" method="POST" autocomplete="off">
                     <div class="form-group">
-                        <input type="email" class="form-control" placeholder="Recipients">
+                        <input type="email" name="email" class="form-control" placeholder="Recipient">
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Subject">
+                        <input type="text" name="subject" class="form-control" placeholder="Subject">
                     </div>
                     <div class="form-group">
-                        <textarea name="" cols="30" rows="10" class="form-control textarea" placeholder="Enter your message"></textarea>
+                        <textarea name="message" cols="30" rows="10" class="form-control textarea" placeholder="Enter your message"></textarea>
                     </div>
                     <div class="form-group">
-                        <input type="submit" class="form-control button btn-primary" value="Send">
+                        <input type="submit" name="send" class="form-control button btn-primary" value="Send">
                     </div>
                 </form>
             </div>
